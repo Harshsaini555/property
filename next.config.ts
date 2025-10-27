b0fc4fe40344c433d1557eaa5a5af5a3d91f9b2f
Harsh Saini <harshsaini132159@gmail.com>
2025-10-28 00:03:19 +0530

    change in config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // 👈 Important for static export
+  images: {
+    unoptimized: true, // required if using next/image
+  },
 };
 
 export default nextConfig;
