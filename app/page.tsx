f4464da2b7f5a0a928471c283b8899c954e968a9
Harsh Saini <harshsaini132159@gmail.com>
2025-11-10 11:47:07 +0530

    client side update 4

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-import Header from "@/components/landing/Header";
-import Hero from "@/components/landing/Hero";
-import Counters from "@/components/landing/Counters";
-import Services from "@/components/landing/Services";
-import FeaturedProperties from "@/components/landing/FeaturedProperties";
-import Agents from "@/components/landing/Agents";
-import YouTube from "@/components/landing/YouTube";
-import Testimonials from "@/components/landing/Testimonials";
-import {ContactFAQ} from "@/components/landing/ContactFAQ";
-import Footer from "@/components/landing/Footer";
-import WhatsAppButton from "@/components/ui/WhatsAppButton";
+import Header from "../components/landing/Header";
+import Hero from "../components/landing/Hero";
+import Counters from "../components/landing/Counters";
+import Services from "../components/landing/Services";
+import FeaturedProperties from "../components/landing/FeaturedProperties";
+import Agents from "../components/landing/Agents";
+import YouTube from "../components/landing/YouTube";
+import Testimonials from "../components/landing/Testimonials";
+import {ContactFAQ} from "../components/landing/ContactFAQ";
+import Footer from "../components/landing/Footer";
+import WhatsAppButton from "../components/ui/WhatsAppButton";
 import StickyBannerDemo from "../components/landing/sticky";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen overflow-x-hidden w-full bg-white">
       <StickyBannerDemo />
       <Header />
       <Hero />
